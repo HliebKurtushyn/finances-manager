587b02f8186886750bdb4e5829cf0e27a6a65ee4
GlebKurt <kurtgleb007@gmail.com>
2025-04-07 18:23:01 +0200

    Just testing

--- Project/main.py
+++ Project/main.py
@@ -1,3 +1,20 @@
 from subprocess import run
 from json_funcs import dump_to_json, load_from_json, create_settings, restore_settings
 from auth import register, login
+from transactions import transaction
+from finances import see_transactions_list
+
+currencies_db = load_from_json('currencies')
+users_db = load_from_json('users')
+balances_db = load_from_json('balances')
+users_card = "1234567890123456"
+username = 'gleb'
+logged = True
+safe_transactions = True
+users_currency = 'EUR'
+users_details_db = load_from_json('users_details')
+transactions_db = load_from_json('transactions')
+
+transaction(currencies_db, users_db, balances_db, users_card, username, logged, safe_transactions, users_currency, users_details_db, transactions_db)
+
+see_transactions_list(transactions_db, username)
