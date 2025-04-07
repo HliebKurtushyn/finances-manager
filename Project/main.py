57bb8eaf395abd7ccb91cc97993e01cd9653a66e
GlebKurt <kurtgleb007@gmail.com>
2025-04-07 19:02:20 +0200

    Just testing thinks

--- Project/main.py
+++ Project/main.py
@@ -2,7 +2,7 @@ from subprocess import run
 from json_funcs import dump_to_json, load_from_json, create_settings, restore_settings
 from auth import register, login
 from transactions import transaction
-from finances import see_transactions_list
+from finances import view_transactions, edit_transaction
 
 currencies_db = load_from_json('currencies')
 users_db = load_from_json('users')
@@ -14,7 +14,3 @@ safe_transactions = True
 users_currency = 'EUR'
 users_details_db = load_from_json('users_details')
 transactions_db = load_from_json('transactions')
-
-transaction(currencies_db, users_db, balances_db, users_card, username, logged, safe_transactions, users_currency, users_details_db, transactions_db)
-
-see_transactions_list(transactions_db, username)
